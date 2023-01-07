89bb79b686ec70c4129299e2c212784f0779101c
ghassewooof <121681428+ghassewooof@users.noreply.github.com>
2023-01-07 11:03:28 +0100

    Add files via upload

---
+++ calculatuer/src/caluclatur.py
@@ -0,0 +1,30 @@
+x = input ("choese + or - or * or / :")
+
+if ( x ==  "+"):
+
+    c = int(input ("enter namber 1 :"))
+    f = int(input ("enter namber 2 :"))
+
+    result = c + f
+    print(result)
+
+if ( x == "-"):
+    c = int(input ("enter namber 1 :"))
+    f = int(input ("enter namber 2 :"))
+
+    result = c - f
+    print(result)
+
+if ( x == "*"):
+    c = int(input ("enter namber 1 :"))
+    f = int(input ("enter namber 2 :"))
+
+    result = c * f
+    print(result)
+
+if ( x == "/"):
+    c = int(input ("enter namber 1 :"))
+    f = int(input ("enter namber 2 :"))
+
+    result = c / f
+    print(result)
